f03845027e07059938534530e6f90a7f8e73c78d
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-04-25 18:23:39 +0530

    Update sender.h

--- sender/sender.h
+++ sender/sender.h
@@ -2,4 +2,4 @@
 
 void ReadBMSParametersFromFile(float* Temperature, float* SOC, float* ChargeRate);
 void PrintBMSParamsOnConsole(float* Temperature, float* SOC, float* ChargeRate);
-void sendBMSParameters();
+void SendBMSParameters();
