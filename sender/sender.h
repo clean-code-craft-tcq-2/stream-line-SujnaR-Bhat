59a5ac765320214e7fbf53dbdba876b056c21d22
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-04-25 19:09:58 +0530

    Update sender.h

--- sender/sender.h
+++ sender/sender.h
@@ -1,5 +1,5 @@
 #define TotalSamples 50
 
-void ReadBMSParametersFromFile(float* Temp, float* SOC, float* ChrgRt);
+void ReadBMSParametersFromInputFile(float* Temp, float* SOC, float* ChrgRt);
 void PrintBMSParamsOnConsole(float* Temp, float* SOC, float* ChrgRt);
 void SendBMSParameters();
