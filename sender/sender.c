a7228706a534f5d23299ac92128e373956e9d77f
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-04-25 18:31:14 +0530

    Update sender.c

--- sender/sender.c
+++ sender/sender.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include "sender.h"
 
-void ReadBMSParametersFromFile(float* Temperature, float* SOC, float* ChargeRate)
+void ReadBMSParametersFromFile(float* Temp, float* SOC, float* ChrgRt)
 {
     float Temperature, StateOfCharge, ChargeRate;
     FILE * Inputfile= fopen("./sender/sender.txt","r");  
     if (Inputfile!=NULL) 
     {
-        for(int i=0;fscanf(file, "%f\t%f\t%f\n", &Temperature,&StateOfCharge,&ChargeRate)!=EOF ;i++)
+        for(int i=0;fscanf(Inputfile, "%f\t%f\t%f\n", &Temperature,&StateOfCharge,&ChargeRate)!=EOF ;i++)
         {
-            *(Temperature+i) = Temperature;
+            *(Temp+i) = Temperature;
             *(SOC+i)  = StateOfCharge;
-            *(ChargeRate+i)   = ChargeRate;
+            *(ChrgRt+i)   = ChargeRate;
         }
     }
     fclose(Inputfile);  
 }
 
-void PrintBMSParametersOnConsole(float* Temperature, float* SOC, float* ChargeRate)
+void PrintBMSParametersOnConsole(float* Temp, float* SOC, float* ChrgRt)
 {
   for (int i=0; i<TotalSamples; i++)
   {
-    printf(" Temperature:%.2f deg C, State of Charge:%.2f, Charge Rate:%.2f\n",  *(Temperature+i),*(SOC+i),*(ChargeRate+i));
+    printf(" Temperature:%.2f deg C, State of Charge:%.2f, Charge Rate:%.2f\n",  *(Temp+i),*(SOC+i),*(ChrgRt+i));
   }
 }
 
@@ -30,6 +30,6 @@ void SendBMSParameters()
   float Temperature[TotalSamples] = {0};
   float StateOfCharge[TotalSamples]= {0};
   float ChargeRate[TotalSamples] = {0};
-  ReadBMSParametersFromFile(Temperature, StateOfCharge, ChargeRate);
-  PrintBMSParametersOnConsole(Temperature, StateOfCharge, ChargeRate);
+  ReadBMSParametersFromFile(Temp, StateOfCharge, ChrgRt);
+  PrintBMSParametersOnConsole(Temp, StateOfCharge, ChrgRt);
 }
