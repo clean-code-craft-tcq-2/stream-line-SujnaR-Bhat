964d3746a715edd4fdd6f0d7bf3257554a567779
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-05-06 17:47:57 +0530

    Update test_BMS_sender.cpp

--- test_BMS_sender.cpp
+++ test_BMS_sender.cpp
@@ -7,16 +7,14 @@ TEST_CASE("Check whether reading and passing of the data is successful")
 {
   float Temperature[TotalSamples] = {0};
   float StateOfCharge[TotalSamples] = {0};
-  float ChargeRate[TotalSamples] = {0};
-  float ExpectedOutput[3][3] = {{2,5,0.0}, {5,5,0.0}, {10,5,0.1}};
+  float ExpectedOutput[2][3] = {{2,5}, {5,5}, {10,5}};
   
-  ReadBMSParametersFromInputFile(Temperature,StateOfCharge,ChargeRate);
+  ReadBMSParametersFromInputFile(Temperature,StateOfCharge);
   int j= 0, i= 0;
   for(i=0;i<3;i++)
   {
     REQUIRE(Temperature[j] == ExpectedOutput[i][0]);
     REQUIRE(StateOfCharge[j] == ExpectedOutput[i][1]);
-    REQUIRE(ChargeRate[j] == ExpectedOutput[i][2]);
     j++;
   }
     SendBMSParameters();
