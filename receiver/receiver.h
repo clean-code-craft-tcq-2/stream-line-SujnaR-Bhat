d80bb7c4cf054a8da29acf338c8417c0caf0df77
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-05-09 18:33:06 +0530

    Update receiver.h

--- receiver/receiver.h
+++ receiver/receiver.h
@@ -1,12 +1,12 @@
 void ReadBMSParametersfromConsole(float* Temp,float* SOC , int TotalSamples);
 
-float CalculateMinValue((float* InputData , int TotalSamples);
+float CalculateMinValue(float* InputData , int TotalSamples);
 
 float CalculateMaxValue(float* InputData , int TotalSamples);
                         
 float CalculateAvg(float* InputData, int TotalSamples);
                         
-void CalculateSMA(float *input, int windowsize, float *movingAvg);
+void CalculateSMA(float *InputData, int windowsize, float *movingAvg);
                    
 void PrintMinMaxValues(float* Temp, float* SOC, int TotalSamples) ;
                         
