8e7f472365b8c6ac0337b2e1128d16bc38571aed
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-05-09 15:47:06 +0530

    Update and rename testBMSreceiver to testBMSreceiver.cpp

---
+++ testBMSreceiver.cpp
@@ -0,0 +1,16 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "test/catch.hpp"
+#include "receiver.h"
+#include <string.h>
+#include <stdio.h>
+
+TEST_CASE("Test for finding Max , Min and Avg value") 
+{
+  float temp[5]= {2, 5, 10, 15, 20};
+  float Max = CalculateMaxValue(temp, 5);
+  float Min = CalculateMinValue(temp, 5);
+  float Avg = CalculateAvg(temp , 5);
+  REQUIRE(Max == 20f);
+  REQUIRE(Min == 2f);
+  REQUIRE( abs(Avg - 10.4f ) <=0.001);
+}
